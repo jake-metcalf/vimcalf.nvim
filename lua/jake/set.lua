fd23a0013cb6703e015f7e0ead21d30397336d22
Jake Brown <jake@Jakes-MacBook-Air.local>
2025-03-10 22:04:13 +1100

    chore: lint

--- lua/jake/set.lua
+++ lua/jake/set.lua
@@ -5,7 +5,7 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
@@ -15,7 +15,7 @@ vim.opt.showmode = false
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
+    vim.opt.clipboard = "unnamedplus"
 end)
 
 -- Enable break indent
@@ -29,7 +29,7 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 -- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 
 -- Decrease update time
 vim.opt.updatetime = 250
@@ -45,10 +45,10 @@ vim.opt.splitbelow = true
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 -- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
+vim.opt.inccommand = "split"
 
 -- Show which line your cursor is on
 vim.opt.cursorline = true
@@ -62,62 +62,62 @@ vim.g.minigit_disable = true
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
-vim.api.nvim_create_autocmd('LspAttach', {
-  desc = 'LSP attach and Keymaps',
-  group = vim.api.nvim_create_augroup('jake', { clear = true }),
-  callback = function(e)
-    local opts = { buffer = e.buf }
-    vim.keymap.set('n', 'gd', function()
-      vim.lsp.buf.definition()
-    end, opts)
-    vim.keymap.set('n', 'K', function()
-      vim.lsp.buf.hover()
-    end, opts)
-    vim.keymap.set('n', '<leader>vws', function()
-      vim.lsp.buf.workspace_symbol()
-    end, opts)
-    vim.keymap.set('n', '<leader>vd', function()
-      vim.diagnostic.open_float()
-    end, opts)
-    vim.keymap.set('n', '<leader>vca', function()
-      vim.lsp.buf.code_action()
-    end, opts)
-    vim.keymap.set('n', '<leader>vrr', function()
-      vim.lsp.buf.references()
-    end, opts)
-    vim.keymap.set('n', '<leader>vrn', function()
-      vim.lsp.buf.rename()
-    end, opts)
-    vim.keymap.set('i', '<C-h>', function()
-      vim.lsp.buf.signature_help()
-    end, opts)
-    vim.keymap.set('n', '[d', function()
-      vim.diagnostic.goto_next()
-    end, opts)
-    vim.keymap.set('n', ']d', function()
-      vim.diagnostic.goto_prev()
-    end, opts)
-  end,
+vim.api.nvim_create_autocmd("LspAttach", {
+    desc = "LSP attach and Keymaps",
+    group = vim.api.nvim_create_augroup("jake", { clear = true }),
+    callback = function(e)
+        local opts = { buffer = e.buf }
+        vim.keymap.set("n", "gd", function()
+            vim.lsp.buf.definition()
+        end, opts)
+        vim.keymap.set("n", "K", function()
+            vim.lsp.buf.hover()
+        end, opts)
+        vim.keymap.set("n", "<leader>vws", function()
+            vim.lsp.buf.workspace_symbol()
+        end, opts)
+        vim.keymap.set("n", "<leader>vd", function()
+            vim.diagnostic.open_float()
+        end, opts)
+        vim.keymap.set("n", "<leader>vca", function()
+            vim.lsp.buf.code_action()
+        end, opts)
+        vim.keymap.set("n", "<leader>vrr", function()
+            vim.lsp.buf.references()
+        end, opts)
+        vim.keymap.set("n", "<leader>vrn", function()
+            vim.lsp.buf.rename()
+        end, opts)
+        vim.keymap.set("i", "<C-h>", function()
+            vim.lsp.buf.signature_help()
+        end, opts)
+        vim.keymap.set("n", "[d", function()
+            vim.diagnostic.goto_next()
+        end, opts)
+        vim.keymap.set("n", "]d", function()
+            vim.diagnostic.goto_prev()
+        end, opts)
+    end,
 })
 
-vim.api.nvim_create_autocmd('BufWritePre', {
-  pattern = '*',
-  callback = function(args)
-    require('conform').format { bufnr = args.buf }
-  end,
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*",
+    callback = function(args)
+        require("conform").format { bufnr = args.buf, timeout_ms = 5000 }
+    end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "help",
-  command = "wincmd L"
+    pattern = "help",
+    command = "wincmd L",
 })
