378a7fbbcad1fc3695f415fb325399a62c70c92a
Jake Brown <jake@Jakes-MacBook-Air.local>
2025-03-16 15:19:25 +1100

    feat: add supermaven

---
+++ lua/jake/lazy/supermaven.lua
@@ -0,0 +1,9 @@
+return {
+    {
+        "supermaven-inc/supermaven-nvim",
+        cmd = "SupermaverUseFree",
+        config = function()
+            require("supermaven-nvim").setup {}
+        end,
+    },
+}
