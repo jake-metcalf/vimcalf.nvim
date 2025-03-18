df5de545fb7e94630278d23105391748e157a317
Jake Brown <jake@Jakes-MacBook-Air.local>
2025-03-18 12:16:17 +1100

    fix: macro showing

--- lua/jake/lazy/noice.lua
+++ lua/jake/lazy/noice.lua
@@ -7,10 +7,7 @@ return {
     dependencies = {
         -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
         "MunifTanjim/nui.nvim",
-        -- OPTIONAL:
-        --   `nvim-notify` is only needed, if you want to use the notification view.
-        --   If not available, we use `mini` as the fallback
-        "rcarriga/nvim-notify",
+        "hrsh7th/nvim-cmp",
     },
     config = function()
         require("noice").setup {
@@ -29,6 +26,12 @@ return {
                 long_message_to_split = true, -- long messages will be sent to a split
                 inc_rename = false, -- enables an input dialog for inc-rename.nvim
                 lsp_doc_border = false, -- add a border to hover docs and signature help
+            },
+            routes = {
+                {
+                    view = "cmdline",
+                    filter = { event = "msg_showmode" },
+                },
             },
         }
     end,
