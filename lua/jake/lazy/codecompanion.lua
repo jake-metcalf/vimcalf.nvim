e484afef8dbc9c8815c0d81ea7fa8fa054be8ff0
Jake Brown <jake@Jakes-MacBook-Air.local>
2025-10-08 10:34:38 +1100

    fix: codecompanion deprecation

--- lua/jake/lazy/codecompanion.lua
+++ lua/jake/lazy/codecompanion.lua
@@ -6,43 +6,45 @@ return {
     },
     opts = {
         adapters = {
-            openai = function()
-                return require("codecompanion.adapters").extend("openai", {
-                    env = {
-                        api_key = os.getenv "OPENAI_API_KEY_ASX",
-                    },
-                    schema = {
-                        model = {
-                            default = "gpt-4o",
+            http = {
+                openai = function()
+                    return require("codecompanion.adapters").extend("openai", {
+                        env = {
+                            api_key = os.getenv "OPENAI_API_KEY_ASX",
                         },
-                    },
-                })
-            end,
-        },
-        strategies = {
-            chat = {
-                adapter = "openai",
-                tools = {
-                    ["mcp"] = {
-                        -- calling it in a function would prevent mcphub from being loaded before it's needed
-                        callback = function()
-                            return require "mcphub.extensions.codecompanion"
-                        end,
-                        description = "Call tools and resources from the MCP Servers",
-                        opts = {
-                            requires_approval = true,
+                        schema = {
+                            model = {
+                                default = "gpt-4o",
+                            },
+                        },
+                    })
+                end,
+            },
+            strategies = {
+                chat = {
+                    adapter = "openai",
+                    tools = {
+                        ["mcp"] = {
+                            -- calling it in a function would prevent mcphub from being loaded before it's needed
+                            callback = function()
+                                return require "mcphub.extensions.codecompanion"
+                            end,
+                            description = "Call tools and resources from the MCP Servers",
+                            opts = {
+                                requires_approval = true,
+                            },
                         },
                     },
                 },
             },
-        },
-        display = {
-            diff = {
-                enabled = true,
-                close_chat_at = 240,
-                layout = "vertical",
-                opts = { "internal", "filler", "closeoff", "algorithm:patience", "followwrap", "linematch:120" },
-                provider = "default",
+            display = {
+                diff = {
+                    enabled = true,
+                    close_chat_at = 240,
+                    layout = "vertical",
+                    opts = { "internal", "filler", "closeoff", "algorithm:patience", "followwrap", "linematch:120" },
+                    provider = "default",
+                },
             },
         },
     },
